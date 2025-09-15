b733f61f3e374b3c2a341b737d7661c665abb47b
Abhishek Yadav <abhishek26321@gmail.com>
2025-09-15 22:09:23 +0530

    Added http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abh1shekyadav/notification-manager
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := ":8080"
+	fmt.Printf("Starting server on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
